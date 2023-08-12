fe74d16dbcb50b658c418470f3f1abc986a38a8e
Banan-Ibrahim20 <bananastal2018@gmail.com>
2023-08-12 20:16:59 +0300

    Banan Commit2

--- lib/main.dart
+++ lib/main.dart
@@ -1,107 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:prog_jobs_grad/view/screens/CompanyScreens/AcceptPersonScreen.dart';
+import 'package:prog_jobs_grad/view/screens/CompanyScreens/AddNewJobScreen.dart';
+import 'package:prog_jobs_grad/view/screens/CompanyScreens/ArchiveScreen.dart';
+import 'package:prog_jobs_grad/view/screens/CompanyScreens/CompanyInfoEditScreen.dart';
+import 'package:prog_jobs_grad/view/screens/CompanyScreens/CompanyInfoScreen.dart';
+import 'package:prog_jobs_grad/view/screens/CompanyScreens/ConversationScreen.dart';
+import 'package:prog_jobs_grad/view/screens/CompanyScreens/EditJobScreen.dart';
+import 'package:prog_jobs_grad/view/screens/CompanyScreens/MessagesCom.dart';
+import 'package:prog_jobs_grad/view/screens/CompanyScreens/com_all_jobs.dart';
+import 'package:prog_jobs_grad/view/screens/CompanyScreens/com_home.dart';
+import 'package:prog_jobs_grad/view/screens/CompanyScreens/com_logo.dart';
+import 'package:prog_jobs_grad/view/screens/CompanyScreens/no_of_request.dart';
+import 'package:prog_jobs_grad/view/screens/ProgrammerScreen/FavoriteScreen.dart';
+import 'package:prog_jobs_grad/view/screens/ProgrammerScreen/JobDetailsScreen.dart';
+import 'package:prog_jobs_grad/view/screens/ProgrammerScreen/MessagesProg.dart';
+import 'package:prog_jobs_grad/view/screens/ProgrammerScreen/NotificationDetialsScreen.dart';
+import 'package:prog_jobs_grad/view/screens/ProgrammerScreen/NotificationsScreen.dart';
+import 'package:prog_jobs_grad/view/screens/ProgrammerScreen/ProfileInfoEditScreen.dart';
+import 'package:prog_jobs_grad/view/screens/ProgrammerScreen/ProfileInfoScreen.dart';
+import 'package:prog_jobs_grad/view/screens/ProgrammerScreen/SettingScreen.dart';
+import 'package:prog_jobs_grad/view/screens/ProgrammerScreen/SubmitJopScreen.dart';
+import 'package:prog_jobs_grad/view/screens/ProgrammerScreen/all_jobs.dart';
+import 'package:prog_jobs_grad/view/screens/ProgrammerScreen/home.dart';
+import 'package:prog_jobs_grad/view/screens/ProgrammerScreen/request_status.dart';
+import 'package:prog_jobs_grad/view/screens/ProgrammerScreen/splash_screen.dart';
+import 'package:prog_jobs_grad/view/screens/shared_screens/forget_password.dart';
+import 'package:prog_jobs_grad/view/screens/shared_screens/login.dart';
+import 'package:prog_jobs_grad/view/screens/shared_screens/signup.dart';
+import 'package:prog_jobs_grad/view/screens/shared_screens/user_type.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      debugShowCheckedModeBanner: false,
+      initialRoute: SplashScreen.id,
+      routes: {
+        SplashScreen.id: (context) => SplashScreen(),
+        UserTypeScreen.id: (context) => UserTypeScreen(),
+        LoginScreen.id: (context) => LoginScreen(
+              userType: '',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+        SignupScreen.id: (context) => SignupScreen(userType: ''),
+        ForgetPasswordScreen.id: (context) => ForgetPasswordScreen(
+              userType: '',
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        HomeScreen.id: (context) => HomeScreen(),
+        AllJobScreen.id: (context) => AllJobScreen(),
+        RequestStatusScreen.id: (context) => RequestStatusScreen(),
+        NotificationScreen.id: (context) => NotificationScreen(),
+        SettingScreen.id: (context) => SettingScreen(),
+        JobDetailsScreen.id: (context) => JobDetailsScreen(),
+        SubmitJopScreen.id: (context) => SubmitJopScreen(),
+        ComLogoScreen.id: (context) => ComLogoScreen(),
+        NumberOfRequestsScreen.id: (context) => NumberOfRequestsScreen(),
+        ComAllJobScreen.id: (context) => ComAllJobScreen(),
+        ComHomeScreen.id: (context) => ComHomeScreen(),
+        AddNewJobScreen.id: (context) => AddNewJobScreen(),
+        MessageCom.id: (context) => MessagesProg(),
+        MessagesProg.id: (context) => MessagesProg(),
+        EditJobScreen.id: (context) => EditJobScreen(),
+        NotificationDetailsScreen.id: (context) => NotificationDetailsScreen(),
+        ConversationScreen.id: (context) => ConversationScreen(),
+        AcceptPerson.id: (context) => AcceptPerson(),
+        Archive.id: (context) => Archive(),
+        Favorite.id: (context) => Favorite(),
+        CompanyInfoEdit.id: (context) => CompanyInfoEdit(),
+        CompanyInfo.id: (context) => CompanyInfo(),
+        ProfileInfoEdit.id: (context) => CompanyInfoEdit(),
+        ProfileInfo.id: (context) => CompanyInfo(),
+      },
     );
   }
 }
